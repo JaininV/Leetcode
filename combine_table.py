a80175ef0190cf3076f543915d08cef1c4023dc1
JaininV <jaininvekariya@gmail.com>
2025-01-27 16:14:51 -0500

    ready to go

--- combine_table.py
+++ combine_table.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-tab1 = pd.DataFrame({'personId': [1, 2], 'lastName': ['Wang', 'Alice'], 'firstName': ['Allen', 'Bob']})
-tab2 = pd.DataFrame({'addressId': [1, 2], 'personId': [2,3], 'city': ['New York City', 'Bob'], 'state': ['New York', 'California']}) 
+Person = pd.DataFrame({'personId': [1, 2], 'lastName': ['Wang', 'Alice'], 'firstName': ['Allen', 'Bob']})
+Address = pd.DataFrame({'addressId': [1, 2], 'personId': [2,3], 'city': ['New York City', 'Bob'], 'state': ['New York', 'California']}) 
 
 
 #combine two tables
-final_table = tab1.merge(tab2, how='left', on='personId')
-print(final_table[['firstName', 'lastName', 'city', 'state']])
+combine_two_tables = Person.merge(Address, how='left', on='personId')
+print(combine_two_tables[['firstName', 'lastName', 'city', 'state']])
